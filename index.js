967101d7f6aeeb59fdd0ba8a450b18d7008f4d3c
08fdange <08fdange@gmail.com>
2020-08-16 18:35:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {  
+	  return response.json();
+	})
+  .then(function(json) {  
+	  renderBooks(json)
+  });
+  return fetch('https://anapioficeandfire.com/api/books');
 }
 
-function renderBooks(books) {
+function renderBooks(json) {
   const main = document.querySelector('main')
-  books.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+    h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
